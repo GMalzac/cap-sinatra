1afaf8bf09643469890987bb62a798e69fb14076
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-04-16 15:10:24 +0100

    Set ubuntu distro tohe correct version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -37,7 +37,7 @@ gem install rack
 sudo apt-get install -y dirmngr gnupg
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 sudo apt-get install -y apt-transport-https ca-certificates
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger focal main > /etc/apt/sources.list.d/passenger.list'
 sudo apt-get update
 sudo apt-get install -y libnginx-mod-http-passenger
 
