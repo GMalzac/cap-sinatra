02b252c7be743a62febeafc13093383da5c14996
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-04-16 15:56:35 +0100

    Make a slightly more complex API

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
+require 'json'
+require 'digest'
 
 class App < Sinatra::Base
+  before do
+    content_type :json
+  end
+  
   get '/' do
-    p "Hey from Sinatra"
+    { status: :ok}.to_json
+  end
+  
+  get '/:name' do
+    {
+      name: params[:name],
+      digest: Digest::SHA256.base64digest(params[:name])
+    }.to_json
   end
 end
