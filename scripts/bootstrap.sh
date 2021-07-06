e0442feac22d63687c1c15312484c4e6138bb485
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-07-06 18:58:08 +0100

    Add logging

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+echo '===================== boostrap.sh'
 ssh cap-sinatra "mkdir -p ~/config"
 scp ./config/* cap-sinatra:~/config
-
+echo '===================== ./scripts/setup.sh cap-sinatra'
 scp ./scripts/setup.sh cap-sinatra:~/
+echo '===================== cap-sinatra chmod +x ~/setup.sh'
 ssh cap-sinatra "chmod +x ~/setup.sh" 
+echo '===================== ~/setup.sh'
 ssh cap-sinatra "~/setup.sh"
