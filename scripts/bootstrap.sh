34cdd6cb7eab70ef57e84db8b9083d0689ed1467
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-12-10 15:26:52 +0000

    Remove logging

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo '===================== boostrap.sh'
+
 ssh cap-sinatra "mkdir -p ~/config"
 scp ./config/* cap-sinatra:~/config
-echo '===================== ./scripts/setup.sh cap-sinatra'
+
 scp ./scripts/setup.sh cap-sinatra:~/
-echo '===================== cap-sinatra chmod +x ~/setup.sh'
 ssh cap-sinatra "chmod +x ~/setup.sh" 
-echo '===================== ~/setup.sh'
 ssh cap-sinatra "~/setup.sh"
